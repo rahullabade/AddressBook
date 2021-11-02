25011a49bbdb70d58959fa4d63aea680d26f2612
RAHUL LABADE <rahullabade05@gmail.com>
2021-11-02 19:48:44 +0530

    Added: UC1_AddressBook

--- Main.java
+++ Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to AddressBook");
+        System.out.println("Welcome to Address Book Program");
+
+        PersonDetails person = new PersonDetails();
+        person.Name("Rahul");
+        person.LastName("Labade");
+        person.Address("Shivaji Nagar");
+        person.City("Amravati");
+        person.State("Maharashtra");
+        person.Zip(798456);
+        person.PhoneNumber("789456");
+        person.Email("123@gmail.com");
+        System.out.println(person.toString());
     }
 }
